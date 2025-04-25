81746069656d3d007c64c08842706c2c3fd686c7
Daniil <koloskovmsu@gmail.com>
2025-04-25 15:33:44 +0300

    Добавлены исключения: Auto_params_selection_Error

--- exceptions/exceptions.py
+++ exceptions/exceptions.py
@@ -1,3 +1,7 @@
 class Real_Forecast_Error(Exception):
     """Ошибка, возникающая при посторение реального прогноза моделями"""
     pass
+
+class Auto_params_selection_Error(Exception):
+    """Ошибка, возникающая при автоматическом выборе параметров для базовых функций прогнозирования"""
+    pass
