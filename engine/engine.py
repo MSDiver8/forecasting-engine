b96f38e1609da411af41dc9c41b4655bc8656cb0
Daniil <koloskovmsu@gmail.com>
2025-05-12 18:11:43 +0300

    добавлены базовые модели RWS, RWD, RWDS, TS, ARIMA

--- engine/engine.py
+++ engine/engine.py
@@ -21,10 +21,37 @@ class Forecasting:
             'TS': ['df', 'Forecast_horizon'],
             'ARIMA': ['df', 'Forecast_horizon', 'Frequency']
         }
-
-
-    
+   
     def RW_real_forecast(Data: pd.DataFrame,
                          Forecast_horizon: int) -> pd.DataFrame:
         '''Запускаем модель RW реального прогноза'''
         return models.RW_real_forecast(Data, Forecast_horizon)
+    
+    def RWD_real_forecast(Data: pd.DataFrame,
+                          Forecast_horizon: int,
+                          Frequency: str) -> pd.DataFrame:
+        '''Запускаем модель RWD реального прогноза'''
+        return models.RWD_real_forecast(Data, Forecast_horizon, Frequency)
+    
+    def RWS_real_forecast(Data: pd.DataFrame,
+                          Forecast_horizon: int,
+                          Seasonality: int) -> pd.DataFrame:
+        '''Запускаем модель RWS реального прогноза'''
+        return models.RWS_real_forecast(Data, Forecast_horizon, Seasonality)
+    
+    def RWDS_real_forecast(Data: pd.DataFrame,
+                           Forecast_horizon: int,
+                           Seasonality: int) -> pd.DataFrame:
+        '''Запускаем модель RWDS реального прогноза'''
+        return models.RWDS_real_forecast(Data, Forecast_horizon, Seasonality)
+    
+    def TS_real_forecast(Data: pd.DataFrame,
+                         Forecast_horizon: int) -> pd.DataFrame:
+        '''Запускаем модель TS реального прогноза'''
+        return models.TS_real_forecast(Data, Forecast_horizon)
+    
+    def ARIMA_real_forecast(Data: pd.DataFrame,
+                            Forecast_horizon: int,
+                            Frequency: str) -> pd.DataFrame:
+        '''Запускаем модель ARIMA реального прогноза'''
+        return models.ARIMA_real_forecast(Data, Forecast_horizon, Frequency)
